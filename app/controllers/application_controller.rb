82b7ac0f9c973e2b3a54e9c0f19e52aa52d8c85d
lemonjai <leocheung_1986@hotmail.com>
2016-02-21 03:44:51 -0500

    fixed the sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-  @current_user ||= User.find_by(id: cookies[:user_id]) if cookies[:user_id]
-end
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 
-helper_method :current_user
-
+  helper_method :current_user
 end
